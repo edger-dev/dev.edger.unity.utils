38fa967306729a77928931e8956e7a91011dc487
YJ Park <yjpark@gmail.com>
2023-04-25 22:31:48 +0800

    add hack solution for init only setter in records

---
+++ Runtime/Hack/InitOnlySetter.cs
@@ -0,0 +1,8 @@
+// To use init user setters, need a hack in Unity3d currently.
+// https://learn.microsoft.com/en-us/dotnet/csharp/language-reference/proposals/csharp-9.0/init
+// https://docs.unity3d.com/2021.2/Documentation/Manual/CSharpCompiler.html
+
+namespace System.Runtime.CompilerServices {
+    class IsExternalInit {
+    }
+}
